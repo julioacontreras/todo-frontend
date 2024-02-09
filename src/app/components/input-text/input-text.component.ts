cded76f8fc47b9e5dd43ed238cfebc884abe8017
Julio Alberto Aranis Contreras <julioalbertoaraniscontreras@MacBook-Pro-de-Julio.local>
2024-02-09 11:25:04 +0100

    refact: refactored input text

--- src/app/components/input-text/input-text.component.ts
+++ src/app/components/input-text/input-text.component.ts
@@ -1,26 +1,28 @@
 import { Component, Input, Output, EventEmitter, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'input-text',
   standalone: true,
   host: {'class': 'w-full'},
-  templateUrl: './input-text.component.html'
+  templateUrl: './input-text.component.html',
+  imports: [ FormsModule ]
 })
 export class InputTextComponent {
   @Input() placeHolder = '' 
   @Output() addItem = new EventEmitter<string>();
 
-  value = signal('')
+  value = ''
 
   setValue (event: KeyboardEvent) {
-    this.value.set((event.target as HTMLInputElement).value)
+    this.value = (event.target as HTMLInputElement).value
   }
 
   click() {
-    if (!this.value()) {
+    if (!this.value) {
       return
     }
-    this.addItem.emit(this.value())
-    this.value.set('')
+    this.addItem.emit(this.value)
+    this.value = ''
   }
 }
